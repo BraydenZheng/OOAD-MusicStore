bca94e9da8130a0f0ea10aaac4b128d062595963
BraydenZheng <braydenzheng@github.com>
2022-03-07 23:00:18 -0700

    perf: Maven manage

---
+++ src/main/java/com/OOAD/UnitTest.java
@@ -0,0 +1,20 @@
+package com.OOAD;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Brayden
+ * @create 3/7/22 10:44 PM
+ * @Description
+ */
+public class UnitTest
+{
+	@Test
+	public void addTest() {
+		int a = 1;
+		int b = 1;
+		assertEquals(a+b, 2);
+	}
+}
