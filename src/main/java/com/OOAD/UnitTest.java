e599a2df773bcdf5c58e2050b6595725514275d1
BraydenZheng <braydenzheng@github.com>
2022-03-08 23:26:15 -0700

    feat: unit test

--- src/main/java/com/OOAD/UnitTest.java
+++ src/main/java/com/OOAD/UnitTest.java
@@ -1,7 +1,12 @@
 package com.OOAD;
 
+import com.OOAD.Command.BuyCommand;
+import com.OOAD.Command.Command;
+import com.OOAD.Command.SellCommand;
+import com.OOAD.Command.TimeCommand;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -11,10 +16,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class UnitTest
 {
+
 	@Test
-	public void addTest() {
-		int a = 1;
-		int b = 1;
-		assertEquals(a+b, 2);
+	public void TestStoreNumber() {
+		Simulation s = new Simulation();
+		int storeNumber = s.storeList.size();
+		assertEquals(storeNumber, 2);
 	}
+
+	@Test
+	public void TestCommand() {
+		Simulation s = new Simulation();
+		Command time = new TimeCommand(s.interactionStore);
+		s.interactionStore.setCommand(time);
+		assertEquals(s.interactionStore.currentCommand, time);
+	}
+
+	@Test
+	public void TestSellCommand() {
+		Simulation s = new Simulation();
+		Command sell = new SellCommand(s.interactionStore);
+		s.interactionStore.setCommand(sell);
+		assertEquals(s.interactionStore.currentCommand, sell);
+	}
+
+	@Test
+	public void TestBuyCommand() {
+		Simulation s = new Simulation();
+		Command buy = new BuyCommand(s.interactionStore);
+		s.interactionStore.setCommand(buy);
+		assertEquals(s.interactionStore.currentCommand, buy);
+	}
+
+	@Test
+	public void TestStoreName() {
+		String north = "northSide";
+		Store a = new Store(north);
+		assertEquals(a.name, north);
+	}
+
 }
